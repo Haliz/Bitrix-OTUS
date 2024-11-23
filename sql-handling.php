bcdc280f54ef1bb77e5435161485d4b645e7bd89
Haliz <foril2n1@rambler.ru>
2024-11-23 22:02:09 +0300

    chore : Примеры использования класса Diag

---
+++ sql-handling.php
@@ -0,0 +1,28 @@
+<?php
+
+use Bitrix\Main\Application;
+use Bitrix\Main\Diag\Debug;
+use \Bitrix\Main\Loader;
+
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
+/**
+ * @var CMain $APPLICATION
+ */
+$APPLICATION->setTitle('Отладка SQL');
+
+Loader::includeModule('iblock');
+Application::getConnection()->startTracker();
+
+$query = \Bitrix\Iblock\ElementTable::getList([
+	'filter' => [
+		'IBLOCK_ID' => 1,
+	],
+	'select' => [
+		'ID'
+	],
+]);
+
+Application::getConnection()->stopTracker();
+Debug::dump($query->getTrackerQuery()->getSql());
+
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
